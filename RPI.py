88d465029e23295eab0905a809d2bb52cd063619
Moaaz <moaaz5799@gmail.com>
2021-12-13 17:20:09 +0200

    RPI SOCKET Configurations

--- RPI.py
+++ RPI.py
@@ -1,11 +1,12 @@
 import socket
 import time
 import threading
-#import io
+# import io
 import serial
 import pynmea2
 import serial
 import os
+
 
 # add Semaphore for Location
 def current_location():
@@ -16,15 +17,17 @@ def current_location():
 
     time.sleep(1.1)
     location = ""
-    #needs edit
+    # needs edit
     for i in range(0, 6):
         location = (ser.readline().decode('utf-8'))
     ser.write(b'AT+GPSRD=0\r')
+    x = ser.read(1000)
     print(location)
     msg = pynmea2.parse(location)
     # improve convert msg.lat
-    var_Location = (str(convert_lat(msg.lat)) +" °"+ msg.lat_dir +","+str(convert_long(msg.lon)) + " °"+ msg.lon_dir )
-    #print(msg.lon)
+    var_Location = (
+                str(convert_lat(msg.lat)) + " °" + msg.lat_dir + "," + str(convert_long(msg.lon)) + " °" + msg.lon_dir)
+    # print(msg.lon)
     print(getlocation_link(convert_lat(msg.lat), convert_long(msg.lon)))
 
 
@@ -43,7 +46,7 @@ def send():
     while True:
         send_socket.sendto(message, ('<broadcast>', 5037))
         print("message sent! \n")
-        #Sleep for 1 second
+        # Sleep for 1 second
         time.sleep(1)
 
 
@@ -53,13 +56,15 @@ def receive():
 
     hostName = socket.gethostbyname('0.0.0.0')
     rev_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rev_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    # rev_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     rev_socket.bind((hostName, PORT_NUMBER))
     print("Test server listening on port {0}\n".format(PORT_NUMBER))
 
     while True:
         (data, addr) = rev_socket.recvfrom(SIZE)
         data1 = data.decode('utf-8')
-        print(data1 + " From	" + str(addr) +"\n")
+        print(data1 + " From	" + str(addr) + "\n")
 
 
 def init():
@@ -76,7 +81,6 @@ def init():
             break
 
 
-
 def checkOK():
     ser.write(b'AT+GPSRD=1\r')
     while 1:
@@ -88,6 +92,7 @@ def checkOK():
         if rcv == b'OK\r\n':
             print("GPSRD ON")
             break
+
 
 # def checkOKGPSON():
 #     while 1:
@@ -101,36 +106,39 @@ def checkOK():
 #             break
 
 def convert_lat(x):
-    degree =float(str(x)[:2])
-    minutes =float(str(x)[2:])
-    result = degree+(minutes/60)
+    degree = float(str(x)[:2])
+    minutes = float(str(x)[2:])
+    result = degree + (minutes / 60)
     return result
-def getlocation_link(lat,lon):
-    link = "http://maps.google.com/maps?q=loc:" + str(lat) + ","+str(lon)
+
+
+def getlocation_link(lat, lon):
+    link = "http://maps.google.com/maps?q=loc:" + str(lat) + "," + str(lon)
     return link
 
+
 def convert_long(x):
-    degree =float(str(x)[:3])
-    minutes =float(str(x)[3:])
-    result = degree+(minutes/60)
+    degree = float(str(x)[:3])
+    minutes = float(str(x)[3:])
+    result = degree + (minutes / 60)
     return result
+
 
 if __name__ == "__main__":
     # creating threads
     rev_thread = threading.Thread(target=receive)
     send_thread = threading.Thread(target=send)
 
-    #open Serial for COM 3 and baud rate 115200
-    ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+    # open Serial for COM 3 and baud rate 115200
+    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
 
-    #initialize GPS
+    # initialize GPS
     init()
 
     # starting thread 1 for Receiving
     rev_thread.start()
     # starting thread 2 Main Thread
     send_thread.start()
-    
+
     time.sleep(60)
     exit()
-            
---
+++ launcher.sh
@@ -0,0 +1,5 @@
+cd /
+cd /home/pi/Desktop/Graduation-Project-V2V
+sleep 15
+sudo python3 RPI.py
+cd /
