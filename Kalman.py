1ece74470ee81b3db82669113bbddfbc657267e5
sarahashraf200 <62209940+sarahashraf200@users.noreply.github.com>
2022-06-27 20:36:46 +0200

    erasing uodate comments

--- Kalman.py
+++ Kalman.py
@@ -96,42 +96,27 @@ class kalmanTrack(object):
 
 
     def update(self, z: bcnSample, tm) -> None:
-
-
         zVec = np.zeros((2, 1))
         self.vehId = z.vehId
         # self.pseudonym = z.psym
         self.lifeTime += 1
         self.bActive = True
         self.lastUpdateTime = tm
-
         x_prior = self.X
         p_prior = self.P
-
-      #  self.S = np.dot((np.dot(self.H, p_prior)), (self.H.transpose())) + self.R
         self.S = self.R + np.dot(self.H, np.dot(p_prior, self.H.T))
-
         self.Sinv = np.linalg.inv(self.S)
         self.Sdet = np.linalg.det(self.S)
-
         self.K = np.dot((np.dot(p_prior, self.H.T)), self.Sinv)
-
         zVec = np.array([z.px, z.vx])
-
         temp = (x_prior[0:3]) + np.dot(self.K, zVec - np.dot(self.H, (x_prior[0:3])))
-
         self.X[0] = temp[0]
         self.X[1] = temp[1]
         self.X[2] = temp[2]
-
         zVec = np.array([z.py, z.vy])  # momkn deh 8alat
-
         temp = (x_prior[3:6]) + np.dot(self.K, zVec - np.dot(self.H, (x_prior[3:6])))
-
         self.X[3] = temp[0]
         self.X[4] = temp[1]
         self.X[5] = temp[2]
-
         ik = np.eye(len(self.K), M=len(self.K)) - np.dot(self.K, self.H)
-
         self.P = np.dot((np.dot(ik, p_prior)), ik.transpose()) + np.dot((np.dot(self.K, self.R)), self.K.transpose())
